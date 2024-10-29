00265fd2d6aff01b5b6cb8b0019a2cdbf9f3c4d6
DomainNameExpansion <50421732+nocussing15@users.noreply.github.com>
2024-10-28 21:25:10 -0700

    Finished Exercise
    Exercise has been completed. Things learned from this excercise is:
    - used parseInt() to ensure parameter when be changed to int type
    - const foof = function() {} is a known as function expression, different from
        function declaration
    -needed to ensure variable being appended to had an "" empty value assigned.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let result = "";                        //empty string value 
+    num = parseInt(num) //will ensure num is of int type 
+    if (num >= 0)
+    {
+        for (let i =0; i < num; i++)        //will concat string word together "num" number of times. 
+        {
+            result += string;
+        }
+    
+        return result; 
 
+    }
+
+    else 
+    result = "ERROR";  
+    return result; 
+    
 };
 
 // Do not edit below this line
