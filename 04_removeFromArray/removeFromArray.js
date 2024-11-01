b2bbdaa6792b2c786a626bee2b6afdbc51fc8985
DomainNameExpansion <50421732+nocussing15@users.noreply.github.com>
2024-10-31 22:07:43 -0700

    Finished exercise 4
    Finished exercise 4 with the use of a outer for loop and an inner while loop.
    The for loop traversed the multi-rest parameter arguement array that is created
    when you may have multiple arguements. While the while loop traversed a copy of
    the original array.
    
    1. Traverse the multi-argument array. When traversing, the while loop will check if the current
    element in multi-arugment array is in the array. We splice out the element in the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,...manyMoreArgs) {
+    let newArray = array;                   //copies the argument array 
 
+
+    for(let l =0; l < manyMoreArgs.length; l++)
+    {
+        let i = 0;
+        while (i<newArray.length)
+        {
+                if(newArray[i] === manyMoreArgs[l])
+                {
+                    newArray.splice(i,1); 
+                }
+                else
+                    i++;
+        }
+    }
+   
+
+    return newArray; 
+};
+removeFromArray([1,2,3,4,5],3);
 // Do not edit below this line
 module.exports = removeFromArray;
