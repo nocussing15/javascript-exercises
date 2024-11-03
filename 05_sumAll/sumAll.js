63348ba5a74f9eb775d0b47533536625add89677
DomainNameExpansion <50421732+nocussing15@users.noreply.github.com>
2024-11-02 23:37:01 -0700

    Finished Exercise 5
    Finished the exercise making use of if, else if conditionals. Unsure
    if there is a range function to be used in javascript.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+let sum = 0; 
 
+if ((num1<0) || (num2<0) || (!Number.isInteger(num1)) || (!Number.isInteger(num2)))
+{
+    return "ERROR";
+}
+else if (num1 < num2)
+{
+    for (let i = num1; num1 < (num2+1); num1++)
+    {
+        sum += num1; 
+    }
+
+    return sum; 
+}
+
+else if (num2 < num1)
+{
+    for (let i = num2; num2 < (num1+1); num2++)
+    {
+        sum+= num2;
+    }
+    return sum; 
+}
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
